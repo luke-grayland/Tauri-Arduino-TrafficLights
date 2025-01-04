d203fd32429876db36343c932e21dad91ce74253
Luke Grayland <lgrayland96@gmail.com>
2025-01-04 22:35:11 +0000

    updated styling

--- src/components/layout/ChannelColumn.tsx
+++ src/components/layout/ChannelColumn.tsx
@@ -1,6 +1,6 @@
 import { Box, Flex, Text } from "@chakra-ui/react"
 import { Slider } from "../ui/slider.tsx";
-import { White } from 'react-dial-knob'
+import { Donut } from 'react-dial-knob'
 
 type ChannelColumnProps = {
     panValue: number,
@@ -14,13 +14,12 @@ const ChannelColumn: React.FC<ChannelColumnProps> = ({panValue, setPanValue}) =>
             <Text textStyle="md">Gain</Text>
             <Slider height="200px" orientation="vertical" defaultValue={[40]} />
             <Box margin="auto">
-              <White diameter={100} min={0} max={50} step={1} value={panValue} onValueChange={setPanValue} ariaLabelledBy={'pan-value'}
-              theme={{ 
-                activeNotchColor: 'red',
-                defaultNotchColor: 'floralwhite'
-                }}>
-                <label id={'pan'}>Pan</label>
-              </White>
+              <Donut diameter={100} min={0} max={50} step={1} value={panValue} onValueChange={setPanValue} ariaLabelledBy={'pan-value'}
+              theme={{
+                donutColor: 'grey',
+                donutThickness: 15,
+                }}/>
+              <Text margin={2} textStyle="md">Pan</Text>
             </Box>
           </Flex>
         </Box>
--- src/components/layout/InputColumn.tsx
+++ src/components/layout/InputColumn.tsx
@@ -10,12 +10,20 @@ type InputColumnProps = {
 const InputColumn: React.FC<InputColumnProps> = ({handleLedChange}) => {
     return (
         <Box background="tomato" width="50%" height="100%" padding="4" margin="auto" >
-            <Flex direction="column" gap="4" height="100%">
-                <LightButton label="Input 1" handleLedChange={handleLedChange} colourName={ColourNames.Green}/>
-                <LightButton label="Input 2" handleLedChange={handleLedChange} colourName={ColourNames.Yellow}/>
-                <LightButton label="Input 3" handleLedChange={handleLedChange} colourName={ColourNames.Red}/>
-                <Text textStyle="md">LP/HP Filter</Text>
-                <Slider width="200px" defaultValue={[30, 60]} margin="auto"/>
+            <Flex direction="column" gap="4" height="100%" justifyContent="space-around">
+                <Text color="black" fontWeight="medium" textStyle="2xl">Controller v1.0</Text>
+                <Box>
+                    <Flex direction="column" gap="4" height="100%" justifyContent={"space-between"}>
+                        <Text textStyle="md">Input Selector</Text>
+                        <LightButton label="Input 1" handleLedChange={handleLedChange} colourName={ColourNames.Green}/>
+                        <LightButton label="Input 2" handleLedChange={handleLedChange} colourName={ColourNames.Yellow}/>
+                        <LightButton label="Input 3" handleLedChange={handleLedChange} colourName={ColourNames.Red}/>
+                    </Flex>
+                </Box>
+                <Box marginBottom={20}>
+                    <Text textStyle="md">LP/HP Filter</Text>
+                    <Slider width="200px" defaultValue={[30, 60]} margin="auto"/>
+                </Box>
             </Flex>
         </Box>
     )
